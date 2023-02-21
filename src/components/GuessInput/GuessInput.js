006e30727edfa377104f4351760842c539981cae
Micael Persson <micael.persson@knowit.se>
2023-02-21 07:34:47 +0100

    Minor tweaks after watching solution

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,23 +1,29 @@
 import React from "react";
 
-function GuessInput({guess, setGuess}) {
-
+function GuessInput({ guess, setGuess }) {
   function handleSubmit(event) {
     event.preventDefault();
-    if (guess.length === 5) {
-      setGuess('')
-      console.log(`Guess: ${guess}`)
-    }
-  };
-
-  function handleChange(event) {
-    setGuess(event.target.value);
+    setGuess("");
+    console.log(`Guess: ${guess}`);
   }
 
   return (
     <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" value={guess} minLength="1" maxLength={5} onChange={handleChange}/>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        required
+        minLength="5"
+        maxLength="5"
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(event) => {
+          const newGuess = event.target.value;
+          setGuess(newGuess);
+        }}
+      />
     </form>
   );
 }
